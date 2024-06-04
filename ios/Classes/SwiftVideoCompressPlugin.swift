fb72ae142fea126a6d4ee6ecf2620630bacbcce1
Knyazev Valeriy <knyazevaler25@gmail.com>
2024-06-04 16:11:16 +0500

    fix: nil error

--- ios/Classes/SwiftVideoCompressPlugin.swift
+++ ios/Classes/SwiftVideoCompressPlugin.swift
@@ -202,7 +202,7 @@ public class SwiftVideoCompressPlugin: NSObject, FlutterPlugin {
         
         let session = getComposition(isIncludeAudio, timeRange, sourceVideoTrack!)
         
-        let exporter = AVAssetExportSession(asset: session, presetName: getExportPreset(quality))!
+        let exporter = AVAssetExportSession(asset: session, presetName: getExportPreset(quality))
         
         exporter.outputURL = compressionUrl
         exporter.outputFileType = AVFileType.mp4
